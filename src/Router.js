84d465d2c36394dcbe1350c999ba1d9f7c938662
Seokho0120@gmail.com <93597794+Seokho0120@users.noreply.github.com>
2022-05-23 10:20:19 +0900

    초기세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import View from "./view";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/view" element={<View />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/view.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function View() {
+  return (
+    <div>
+      <div> 이미지툴 </div>
+    </div>
+  );
+}
